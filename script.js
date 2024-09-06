b49f2d8f776d64e786622d5cc3aa72b2cf97c9f0
CassianoGG <106606553+CassianoGG@users.noreply.github.com>
2024-09-06 08:41:31 -0300

    coigo

---
+++ script.js
@@ -0,0 +1,50 @@
+function leDoTeclado(evento) {
+
+    if (evento.keyCode === cima) {
+        sentidoAtual = cima;
+        
+    }else if(evento.keyCode === baixo){
+        sentidoAtual = baixo;
+
+    }else if(evento.keyCode === esquerda){
+        sentidoAtual = esquerda;
+
+    }else if(evento.keyCode === direita){
+        sentidoAtual = direita;
+
+    }
+
+}
+
+functio deslocamento(){
+
+    if (sentidoAtual === direita){
+        cauda.unshift([cauda[0][0]+15,cauda[0][1]]);
+        cauda.pop();
+
+    }
+
+    if (sentidoAtual === esquerda){
+        cauda.unshift([cauda[0][0]-15,cauda[0][1]]);
+        cauda.pop();
+    }
+
+    if (sentidoAtual === cima){
+        cauda.unshift([cauda[0][0],cauda[0][1]-15]);
+        cauda.pop();
+    }
+
+    if (sentidoAtual === baixo){
+        cauda.unshift([cauda[0][0],cauda[0][1]+15]);
+        cauda.pop();
+    }
+
+}
+
+function desenhaCauda(cauda,raio){
+    for (var i=0; i <cauda.length; i++){
+
+
+    }
+    
+}
